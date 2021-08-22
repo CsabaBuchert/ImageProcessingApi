037e81652e2302dde5e8e3c85843c28749288a7d
csbuchert <csaba.buchert@nng.com>
2021-08-22 11:28:15 +0200

    feat: save generated image as thumbnail

--- src/image/imageProvider.ts
+++ src/image/imageProvider.ts
@@ -4,6 +4,9 @@ import { resolveConfig } from "prettier";
 import sharp from "sharp";
 
 export default class ImageProvider {
+    origImagePath = path.join("images", "full");
+    thumbnailsImagePath = path.join("images", "thumbnails");
+
     public async getImage(
         name: string,
         width: number,
@@ -14,7 +17,9 @@ export default class ImageProvider {
                 .then((data) => resolve(data))
                 .catch((_) => {
                     this.createImage(name, width, height)
-                        .then((data) => resolve(data))
+                        .then((data) => {
+                            resolve(data);
+                        })
                         .catch((error) => reject(error));
                 });
         });
@@ -36,7 +41,7 @@ export default class ImageProvider {
         height: number
     ): Promise<Buffer> {
         return new Promise((resolve, reject) => {
-            const filePath = path.join("images", "full", name);
+            const filePath = path.join(this.origImagePath, name);
             fs.open(filePath, "r", (err, fd) => {
                 if (err) {
                     console.error("file open error: " + err.message);
@@ -44,11 +49,17 @@ export default class ImageProvider {
                     return;
                 }
 
-                fs.readFile(fd, (err, data) => {
+                fs.readFile(fd, async (err, data) => {
                     if (err) {
                         reject(new Error(err?.message));
                     } else {
-                        resolve(this.resizeImage(data, width, height));
+                        const resized_data = await this.resizeImage(
+                            data,
+                            width,
+                            height
+                        );
+                        this.saveImage(resized_data, name, width, height);
+                        resolve(resized_data);
                     }
 
                     fs.closeSync(fd);
@@ -63,5 +74,27 @@ export default class ImageProvider {
         height: number
     ): Promise<Buffer> {
         return await sharp(data).resize(width, height).toBuffer();
+    }
+
+    private async saveImage(
+        data: Buffer,
+        name: string,
+        width: number,
+        height: number
+    ): Promise<void> {
+        return new Promise((resolve, reject) => {
+            const filePath = path.join(
+                this.thumbnailsImagePath,
+                width + "_" + height
+            );
+            fs.mkdirSync(filePath, { recursive: true });
+            fs.writeFile(path.join(filePath, name), data, (err) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve();
+            });
+        });
     }
 }
