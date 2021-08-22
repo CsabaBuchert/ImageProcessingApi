ce18168dcc86954bb4bd148fe0bd638448ac1540
csbuchert <csaba.buchert@nng.com>
2021-08-22 10:33:25 +0200

    Add image processer and send image + add launch.json

---
+++ src/image/imageProvider.ts
@@ -0,0 +1,59 @@
+import fs from "fs";
+import path from "path";
+import { resolveConfig } from "prettier";
+import sharp from "sharp";
+
+export default class ImageProvider {
+    public async getImage(
+        name: string,
+        width: number,
+        height: number
+    ): Promise<Buffer> {
+        return new Promise((resolve, reject) => {
+            this.tryGetFromCache(name, width, height)
+                .then((data) => resolve(data))
+                .catch((_) => {
+                    this.createImage(name, width, height)
+                        .then((data) => resolve(data))
+                        .catch((error) => reject(error));
+                });
+        });
+    }
+
+    private async tryGetFromCache(
+        name: string,
+        width: number,
+        height: number
+    ): Promise<Buffer> {
+        return new Promise((resolve, reject) => {
+            reject(new Error("not implemented"));
+        });
+    }
+
+    private async createImage(
+        name: string,
+        width: number,
+        height: number
+    ): Promise<Buffer> {
+        return new Promise((resolve, reject) => {
+            const filePath = path.join("images", "full", name);
+            fs.open(filePath, "r", (err, fd) => {
+                if (err) {
+                    console.error("file open error: " + err.message);
+                    reject(new Error(err.message));
+                    return;
+                }
+
+                fs.readFile(fd, (err, data) => {
+                    if (err) {
+                        reject(new Error(err?.message));
+                    } else {
+                        resolve(data);
+                    }
+
+                    fs.closeSync(fd);
+                });
+            });
+        });
+    }
+}
--- src/server/imageProcesserServer.ts
+++ src/server/imageProcesserServer.ts
@@ -1,14 +1,47 @@
 import express from "express";
+import ImageProvider from "../image/imageProvider";
 import ServerBase from "./serverBase";
 
 export default class ImageProcesserServer extends ServerBase {
     constructor(port: number) {
         super(port, "image_processer");
+
+        process.on("unhandledRejection", (error) => {
+            console.log("unhandledRejection", error);
+        });
     }
     protected messageReceived?(
         request: express.Request,
         response: express.Response
     ): void {
-        response.send("");
+        const provider = new ImageProvider();
+        try {
+            const image = request.query.filename as unknown as string;
+            const width = parseInt(request.query.width as unknown as string);
+            const height = parseInt(request.query.height as unknown as string);
+
+            if (
+                typeof image != "string" ||
+                typeof width != "number" ||
+                typeof height != "number"
+            ) {
+                response.send("Invalid parameters");
+                return;
+            }
+
+            provider
+                .getImage(image, width, height)
+                .then((data) => {
+                    response.set("Content-Type", "image/jpeg");
+                    response.send(data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                    response.send(err);
+                });
+        } catch (err) {
+            console.log(err);
+            response.send(err);
+        }
     }
 }
